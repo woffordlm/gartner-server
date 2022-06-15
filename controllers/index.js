10089e8d84c25b0acd4641c60f118d474f99a6d4
woffordlm <lukewofford@gmail.com>
2022-06-15 10:42:20 -0400

    simplified router

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const axios = require('axios').default;
 
-
-router.use('/api', apiRoutes);
+router.get("/api/stock/:name", (req,res) => {
+    let name = req.params.name
+     const options = {
+         method: 'GET',
+         url: `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${name}&outputsize=compact&apikey=${process.env.API_KEY}`,
+       };
+       
+       axios.request(options).then(function (response) {
+          return response.data
+       })
+       .then((data) => {
+         res.send(data)
+       })
+       .catch(function (error) {
+         console.error(error);
+       });
+ }
+ )
 
 module.exports = router;
